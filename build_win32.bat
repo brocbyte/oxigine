0aeab2e795d9a59559a746578b33259c7eb38a30
hardworkar <arsmoroz@ya.ru>
2024-07-20 09:57:10 +0700

    got Vulkan loader

--- build_win32.bat
+++ build_win32.bat
@@ -2,13 +2,14 @@
 if not exist build_windows (mkdir build_windows)
 pushd build_windows
 set src_path=..\src\
+set vulkan_sdk_include_path=%VULKAN_SDK%\Include
 
 set win32_libs= user32.lib
 set win32_libs=%win32_libs% gdi32.lib
 
-set link=       -opt:ref        &:: remove unused functions
-set link=%link% -incremental:no &:: full link each time
+set link=       -opt:ref
+set link=%link% -incremental:no
 
-cl -Z7 %src_path%win32_oxigine.c %win32_libs% /link %link% -subsystem:windows,5.2
+cl /I %vulkan_sdk_include_path% -Z7 %src_path%win32_oxigine.c %win32_libs% /link %link% -subsystem:windows,5.2
 
 popd
--- src/win32_oxigine.c
+++ src/win32_oxigine.c
@@ -10,6 +10,10 @@
 #include <stdint.h>
 #include <windows.h>
 
+#include <stdio.h>
+#include <assert.h>
+#include <vulkan/vulkan.h>
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -23,6 +27,7 @@ typedef float f32;
 typedef double f64;
 
 static b32 globalRunning;
+static FILE *logFile;
 
 LRESULT CALLBACK win32WindowsCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   LRESULT result = 0;
@@ -75,6 +80,48 @@ static void win32ProcessMessages() {
   }
 }
 
+/**
+ * https://github.com/KhronosGroup/Vulkan-Loader/blob/main/docs/LoaderInterfaceArchitecture.md
+ * https://github.com/KhronosGroup/Vulkan-Loader/blob/main/docs/LoaderApplicationInterface.md
+ */
+static int win32LoadVulkan() {
+  /* Indirectly Linking to the Loader */
+  HMODULE vulkan = LoadLibraryW(L"vulkan-1.dll");
+  if (!vulkan) {
+    fprintf(logFile, "No vulkan!\n");
+    return -1;
+  }
+
+  typedef PFN_vkVoidFunction vkGetInstanceProcAddrT(VkInstance, const char *);
+  vkGetInstanceProcAddrT *vkGetInstanceProcAddr =
+      (vkGetInstanceProcAddrT *)(GetProcAddress(vulkan, "vkGetInstanceProcAddr"));
+
+  if (!vkGetInstanceProcAddr) {
+    fprintf(logFile, "No vkGetInstanceProcAddr!\n");
+    return -1;
+  }
+
+  typedef VkResult vkEnumerateInstanceLayerPropertiesT(uint32_t *, VkLayerProperties *);
+  vkEnumerateInstanceLayerPropertiesT *vkEnumerateInstanceLayerProperties =
+      (vkEnumerateInstanceLayerPropertiesT *)(vkGetInstanceProcAddr(
+          0, "vkEnumerateInstanceLayerProperties"));
+  if (!vkEnumerateInstanceLayerProperties) {
+    fprintf(logFile, "No vkEnumerateInstanceLayerProperties!\n");
+    return -1;
+  }
+
+  uint32_t propertyCount;
+  assert(vkEnumerateInstanceLayerProperties(&propertyCount, 0) == VK_SUCCESS);
+  VkLayerProperties *pProperties =
+      (VkLayerProperties *)malloc(propertyCount * sizeof(VkLayerProperties));
+  assert(vkEnumerateInstanceLayerProperties(&propertyCount, pProperties) == VK_SUCCESS);
+  for (int i = 0; i < propertyCount; ++i) {
+    fprintf(logFile, "layer %d: %s\n", i, pProperties[i].layerName);
+  }
+  free(pProperties);
+  return 0;
+}
+
 int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
   WNDCLASS WindowClass = {.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
                           .lpfnWndProc = win32WindowsCallback,
@@ -89,8 +136,16 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLi
   if (!window)
     exit(-1);
 
+  logFile = fopen("last_run.log", "w");
+
+  if (win32LoadVulkan() != 0) {
+    exit(-1);
+  }
+
   globalRunning = true;
   while (globalRunning) {
     win32ProcessMessages();
   }
+
+  fclose(logFile);
 }
